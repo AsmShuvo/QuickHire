38d902e12684110833b5a54617d1f08cc6ca050c
AsmShuvo <hoffmanhulk99@gmail.com>
2026-03-10 01:33:43 +0600

    deployed domain attached to client

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "https://quickhire-production-97ff.up.railway.app/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
